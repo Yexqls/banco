3a90b95c412f4972eaecd3bb5b5a2daac0e45bbc
Yexqls <yucli.emmanuel.666@gmail.com>
2025-03-05 21:45:52 -0600

    add hover effect

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,8 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
-        <style>html,
-        body {
+        <style>body {
             height: 100%;
             margin: 0;
         }
@@ -31,24 +30,43 @@
             width: 100%;
             margin-top: auto;
         }
-    </style>
 
+        /*ESTILO LINEA*/
+        .hover-linea {
+            display: inline-block;
+            position: relative;
+            text-decoration: none;
+            cursor: pointer;
+        }
+
+        .hover-linea::after {
+            content: "";
+            position: absolute;
+            left: 0;
+            bottom: -2px;
+            width: 0;
+            height: 2px;
+            background-color: #ffffff;
+            transition: width 0.3s ease;
+        }
+
+        .hover-linea:hover::after {
+            width: 100%;
+        }
     </style>
 </head>
 
 <body>
     <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #146ebe;">
-        <a class="navbar-brand" href="/banco" style="color: white; padding-left: 10px;">
-            <i class="fa-solid fa-house" style="padding-right: 2px;"></i> <b>Banco MX</b>
+        <a class="navbar-brand" href="/banco" style="color: white; padding: 10px">
+            <i class="fa-solid fa-house"></i> <b>Banco MX</b>
         </a>
-        <button type="button" class="btn btn-light">
-            <a href="/usuario/crear" style="text-decoration: none"> <i class="fa-solid fa-user-plus"></i>
-                Crear cliente</a>
-        </button>
-        <button type="button" class="btn btn-light" style="margin-left: 20px ">
-            <a href="/usuarios" style="text-decoration: none"> <i class="fa-solid fa-list"></i>
-                Lista de clientes</a>
-        </button>
+        <a class="hover-linea" href="/usuario/crear" style="text-decoration: none;color:white"> <i
+                class="fa-solid fa-user-plus"></i>
+            Crear cliente</a>
+        <a class="hover-linea" href="/usuarios" style="text-decoration: none; margin-left:16px; color:white"> <i
+                class="fa-solid fa-list"></i>
+            Lista de clientes</a>
 
     </nav>
 
